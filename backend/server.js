dfcdda5b4df00604f4a09d68cbb35954f2ab5e59
Jessica Tabatadze <jessicatabata12@gmail.com>
2026-05-18 23:29:32 -0400

    port fix

--- backend/server.js
+++ backend/server.js
@@ -117,7 +117,7 @@ app.delete("/api/orders/:id", async (req, res) => {
   }
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log("Server running on port 5000");
